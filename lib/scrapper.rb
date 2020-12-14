f668c62b19f944e379193892c3cd58f75de542af
dibdas <dibyandu08das@gmail.com>
2020-12-14 15:16:05 +0530

    added page.rb update main.rb
    added class scrapper

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -1,8 +1,40 @@
 require 'httparty'
 require 'nokogiri'
+require 'pry'
 
-class Scrapper 
-    def initialize
-        
+    def scraper
+        array =[]
+        url = 'https://weworkremotely.com/#job-listings'
+        unparsed_page = HTTParty.get(url) 
+        parsed_page ||= Nokogiri::HTML(unparsed_page.body)
+        job_listing = parsed_page.css('li.feature')
+       # @total = parsed_page.css('h2.rc-NumberOfResultsSection span').text.gsub(/[^\d]/, '').to_i
+        total_jobs = parsed_page.css('p b#total').text.gsub(/[^\d]/, '').to_i
+        page=1
+        per_page = job_listing.count
+        last_page = (total_jobs/per_page)
+        #binding.pry
+        puts total_jobs
+        while page<=last_page
+            puts "Page: #{page}"
+            pagination_url ="https://weworkremotely.com/#job-listings/page=#{page}"
+                job_listing.each do |job|
+                    jobs={
+                            name: job.css('span.company').text,
+                            title: job.css('span.title').text,
+                            feature: job.css('span.featured').text,
+                            role: job.css('span.company').text,
+                            region:job.css('span.region company').text,
+                         }
+                array<<jobs
+            puts "Added: #{jobs[:region]}"
+             end
+            page+=1
+        end
+       # puts array
+        #binding.pry
     end
-end
+
+
+scraper
+
