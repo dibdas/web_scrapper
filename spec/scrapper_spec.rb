023a331064d3df0cd9d2ad5fc144a0b5e6dec331
dibdas <dibyandu08das@gmail.com>
2020-12-14 21:42:06 +0530

    updated scraper_spec.rb

--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -6,8 +6,13 @@ describe Scrapper do
             it 'calls the scraper method in Scrapper class with a true value' do
                 allow(scraper).to receive(:scrape).and_return(true)
                 expect(scraper.scrape).to eql(true)
-              end
+            end
+
+
+            it 'calls the start method in Scraper class with a false value' do
+                allow(scraper).to receive(:start).and_return(true)
+                expect(scraper.start).not_to eql(false)
+            end
         end
-    
 end
             
