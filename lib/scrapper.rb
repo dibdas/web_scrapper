d3b01909f0e5e1cae9862b42fe56bc671322ccdd
dibdas <dibyandu08das@gmail.com>
2020-12-14 19:55:29 +0530

    fixed errors

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -2,39 +2,16 @@ require 'httparty'
 require 'nokogiri'
 require 'pry'
 
-    def scraper
-        array =[]
-        url = 'https://weworkremotely.com/#job-listings'
-        unparsed_page = HTTParty.get(url) 
-        parsed_page ||= Nokogiri::HTML(unparsed_page.body)
-        job_listing = parsed_page.css('li.feature')
-       # @total = parsed_page.css('h2.rc-NumberOfResultsSection span').text.gsub(/[^\d]/, '').to_i
-        total_jobs = parsed_page.css('p b#total').text.gsub(/[^\d]/, '').to_i
-        page=1
-        per_page = job_listing.count
-        last_page = (total_jobs/per_page)
-        #binding.pry
-        puts total_jobs
-        while page<=last_page
-            puts "Page: #{page}"
-            pagination_url ="https://weworkremotely.com/#job-listings/page=#{page}"
-                job_listing.each do |job|
-                    jobs={
-                            name: job.css('span.company').text,
-                            title: job.css('span.title').text,
-                            feature: job.css('span.featured').text,
-                            role: job.css('span.company').text,
-                            region:job.css('span.region company').text,
-                         }
-                array<<jobs
-            puts "Added: #{jobs[:region]}"
-             end
-            page+=1
-        end
-       # puts array
-        #binding.pry
-    end
+class Scrapper
+  attr_reader :url
 
+  def initialize(url)
+    @url = url
+  end
 
-scraper
-
+  def scraper
+    unparsed_page = HTTParty.get(@url)
+    parsed_page ||= Nokogiri::HTML(unparsed_page.body)
+    parsed_page.css('li.feature')
+  end
+end
